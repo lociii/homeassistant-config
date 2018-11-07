6d0cdd117108b53a4e23f9489297a40f32bda154
Jens Nistler <opensource@jensnistler.de>
2018-11-07 21:54:05 +0100

    fix fan automation

--- appdaemon/apps/actionFan/actionFan.py
+++ appdaemon/apps/actionFan/actionFan.py
@@ -2,30 +2,65 @@ import appdaemon.plugins.hass.hassapi as hass
 
 
 class HandleActionFan(hass.Hass):
-    """
-    turn the bathroom fans on when the main bathroom light has been activated.
-    keep them running for two minutes after the main bathroom light has been turned off.
-    """
+
     def initialize(self):
-        self.timer = None
+        self.delayer = self.get_app('util_delayer')
+        self.timer_trigger = None
+        self.timer_cycle = None
+        self.fan = self.args['fan']
+        self.trigger = self.args['trigger']
+        self.delay = self.args['delay']
+        self.indicator_presence = self.args['indicator_presence']
+        self.absent_on_duration = self.args['absent_on_duration']
+        self.absent_off_duration = self.args['absent_off_duration']
 
-        # listen to state changes of triggering device
-        self.listen_state(cb=self.turn_target_device_on, entity=self.args['source_device'], new='on', old='off')
-        self.listen_state(cb=self.turn_target_device_off, entity=self.args['source_device'], new='off', old='on')
+        """
+        turn the bathroom fans on when the main bathroom light has been activated.
+        keep them running for two minutes after the main bathroom light has been turned off.
+        """
+        self.listen_state(cb=self.trigger_on, entity=self.trigger, new='on', old='off')
+        self.listen_state(cb=self.trigger_off, entity=self.trigger, new='off', old='on')
 
-    def turn_target_device_on(self, *args, **kwargs):
-        self.log('{} turned on, now turn on {}'.format(self.args['source_device'], self.args['target_device']))
-        self.turn_on(entity_id=self.args['target_device'])
+        """
+        run a fan on/off cycle when noone's home to exchange more air in the apartment
+        """
+        self.listen_state(self.leaving, self.indicator_presence, new='False', old='True')
+        self.listen_state(self.returning, self.indicator_presence, new='True', old='False')
+
+    def trigger_on(self, *args, **kwargs):
+        self.log('{} turned on, now turn on {}'.format(self.trigger, self.fan))
+        self.delayer.add(hass_func='turn_on', entity_id=self.fan)
 
         # cancel last scheduler
-        if self.timer is not None:
-            self.cancel_timer(self.handler)
+        if self.timer_trigger is not None:
+            self.cancel_timer(self.timer_trigger)
 
-    def turn_target_device_off(self, *args, **kwargs):
-        self.log('{} turned off, turn off {} in 10 minutes'.format(self.args['source_device'], self.args['target_device']))
-        self.timer = self.run_in(callback=self.turn_target_device_off_delayed, seconds=int(self.args['delay'])*60)
+    def trigger_off(self, *args, **kwargs):
+        self.log('{} turned off, turn off {} in {}} minutes'.format(self.trigger, self.fan, self.delay))
+        self.timer_trigger = self.run_in(callback=self.trigger_off_delayed, seconds=int(self.delay) * 60)
 
-    def turn_target_device_off_delayed(self, *args, **kwargs):
-        self.log('10 minutes gone, turn off {}'.format(self.args['target_device']))
-        self.turn_off(entity_id=self.args['target_device'])
-        self.timer = None
+    def trigger_off_delayed(self, *args, **kwargs):
+        self.log('{} minutes over, turn off {}'.format(self.delay, self.fan))
+        self.delayer.add(hass_func='turn_off', entity_id=self.fan)
+        self.timer_trigger = None
+
+    def leaving(self, *args, **kwargs):
+        self.absent_turn_on()
+
+    def returning(self, *args, **kwargs):
+        if self.timer_cycle is not None:
+            self.cancel_timer(self.timer_cycle)
+
+    def absent_turn_on(self, *args, **kwargs):
+        self.log('absent - turn fan {} on'.format(self.fan))
+        self.delayer.add(hass_func='turn_on', entity_id=self.fan)
+
+        # set callback to turn fan back off
+        self.timer_cycle = self.run_in(self.absent_turn_off_fans, self.absent_on_duration * 60)
+
+    def absent_turn_off(self, *args, **kwargs):
+        self.log('absent - turn fan {} off'.format(self.fan))
+        self.delayer.add(hass_func='turn_off', entity_id=self.fan)
+
+        # set callback to turn fan back on
+        self.timer_cycle = self.run_in(self.absent_turn_on_fans, self.absent_off_duration * 60)
--- appdaemon/apps/actionLeavingHome/actionLeavingHome.py
+++ appdaemon/apps/actionLeavingHome/actionLeavingHome.py
@@ -3,39 +3,10 @@ import appdaemon.plugins.hass.hassapi as hass
 
 class HandleActionLeavingHome(hass.Hass):
     def initialize(self):
-        self.delayer = self.get_app('util_delayer')
         self.announce = self.get_app('announce')
-
-        self.fans = self.args['fans']
-        self.fan_on_duration = int(self.args['fan_on_duration'])
-        self.fan_off_duration = int(self.args['fan_off_duration'])
-
-        # indicators
         self.indicator_presence = self.args['indicator_presence']
 
-        # execute when coming home
-        self.listen_state(self.update_status, self.indicator_presence, new='False', old='True')
+        self.listen_state(self.leaving, self.indicator_presence, new='False', old='True')
 
-    def update_status(self, *args, **kwargs):
+    def leaving(self, *args, **kwargs):
         self.announce.speak(message='Komm gesund wieder!')
-        self.absent_turn_on_fans()
-
-    def absent_turn_on_fans(self, *args, **kwargs):
-        self.log('absent - turn fans on')
-
-        # turn fans on
-        for fan in self.fans:
-            self.delayer.add(hass_func='turn_on', entity_id=fan)
-
-        # callback in 10 minutes
-        self.fan_handle = self.run_in(self.absent_turn_off_fans, self.fan_on_duration * 60)
-
-    def absent_turn_off_fans(self, *args, **kwargs):
-        self.log('absent - turn fans off')
-
-        # turn fans off
-        for fan in self.fans:
-            self.delayer.add(hass_func='turn_off', entity_id=fan)
-
-        # callback in 5 minutes
-        self.fan_handle = self.run_in(self.absent_turn_on_fans, self.fan_off_duration * 60)
