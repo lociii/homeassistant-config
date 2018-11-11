72625877b2ff2174b55d0307575aad5fec885579
Jens Nistler <opensource@jensnistler.de>
2018-11-11 15:55:09 +0100

    log after successfully executing the action

--- appdaemon/apps/ActionVacuum/ActionVacuum.py
+++ appdaemon/apps/ActionVacuum/ActionVacuum.py
@@ -14,13 +14,13 @@ class ActionVacuum(hass.Hass):
         self.listen_state(cb=self.deactivate, entity=self.trigger, new='off', old='on')
 
     def activate(self, *args, **kwargs):
-        self.log('cleaning cycle starting')
-
         # check for cleaning finished
         self.listener_done = self.listen_state(cb=self.cleaning_done, entity=self.entity, new='docked')
 
         # start cleaning
         self.call_service('vacuum/start', entity_id=self.entity)
+
+        self.log('cleaning cycle started')
 
     def deactivate(self, *args, **kwargs):
         # cancel finish timer
@@ -28,18 +28,18 @@ class ActionVacuum(hass.Hass):
             self.cancel_listen_event(self.listener_done)
             self.listener_done = None
 
+        # cycle not yet finished, go to dock and wait for next call
         if self.get_state(self.target) == 'off':
-            self.log('cleaning cycle paused')
-
-            # pause and return to dock, waiting to get started again
             self.call_service('vacuum/pause', entity_id=self.entity)
             time.sleep(1)
             self.call_service('vacuum/return_to_base', entity_id=self.entity)
 
-    def cleaning_done(self, *args, **kwargs):
-        self.log('cleaning cycle done')
+            self.log('cleaning cycle paused')
 
+    def cleaning_done(self, *args, **kwargs):
         # mark cycle done
         self.turn_on(self.target)
         # turn off active indicator
         self.turn_off(self.trigger)
+
+        self.log('cleaning cycle done')
