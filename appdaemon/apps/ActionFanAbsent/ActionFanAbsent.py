84d80c4028eff14ab6af2ab07c3f60e870bb33bf
Jens Nistler <opensource@jensnistler.de>
2018-11-11 13:01:49 +0100

    split fan actions in responder and absent fan app

--- appdaemon/apps/ActionFanAbsent/ActionFanAbsent.py
+++ appdaemon/apps/ActionFanAbsent/ActionFanAbsent.py
@@ -1,48 +1,17 @@
 import appdaemon.plugins.hass.hassapi as hass
 
 
-class HandleActionFan(hass.Hass):
-
+class ActionFanAbsent(hass.Hass):
     def initialize(self):
         self.delayer = self.get_app('util_delayer')
-        self.timer_trigger = None
         self.timer_cycle = None
         self.fan = self.args['fan']
-        self.trigger = self.args['trigger']
-        self.delay = self.args['delay']
         self.indicator_presence = self.args['indicator_presence']
         self.absent_on_duration = self.args['absent_on_duration']
         self.absent_off_duration = self.args['absent_off_duration']
 
-        """
-        turn the bathroom fans on when the main bathroom light has been activated.
-        keep them running for two minutes after the main bathroom light has been turned off.
-        """
-        self.listen_state(cb=self.trigger_on, entity=self.trigger, new='on', old='off')
-        self.listen_state(cb=self.trigger_off, entity=self.trigger, new='off', old='on')
-
-        """
-        run a fan on/off cycle when noone's home to exchange more air in the apartment
-        """
         self.listen_state(self.leaving, self.indicator_presence, new='False', old='True')
         self.listen_state(self.returning, self.indicator_presence, new='True', old='False')
-
-    def trigger_on(self, *args, **kwargs):
-        self.log('{} turned on, now turn on {}'.format(self.trigger, self.fan))
-        self.delayer.add(hass_func='turn_on', entity_id=self.fan)
-
-        # cancel last scheduler
-        if self.timer_trigger is not None:
-            self.cancel_timer(self.timer_trigger)
-
-    def trigger_off(self, *args, **kwargs):
-        self.log('{} turned off, turn off {} in {} minutes'.format(self.trigger, self.fan, self.delay))
-        self.timer_trigger = self.run_in(callback=self.trigger_off_delayed, seconds=int(self.delay) * 60)
-
-    def trigger_off_delayed(self, *args, **kwargs):
-        self.log('{} minutes over, turn off {}'.format(self.delay, self.fan))
-        self.delayer.add(hass_func='turn_off', entity_id=self.fan)
-        self.timer_trigger = None
 
     def leaving(self, *args, **kwargs):
         self.absent_turn_on()
--- appdaemon/apps/ActionResponder/ActionResponder.py
+++ appdaemon/apps/ActionResponder/ActionResponder.py
@@ -3,23 +3,48 @@ import appdaemon.plugins.hass.hassapi as hass
 
 class ActionResponder(hass.Hass):
     """
-    trigger on/off actions on a list of devices triggered by a single device
+    trigger on/off actions on a list of devices triggered by a single other device
+    supports on/off delay (in seconds)
 
     might be used to trigger non-digitalstrom devices based on digitalstrom scene changes
     """
     def initialize(self):
         self.delayer = self.get_app('util_delayer')
+        self.timer_on = None
+        self.timer_off = None
 
         self.trigger_entity = self.args['trigger_entity']
         self.action_entities = self.args['action_entities']
+        self.on_delay = self.args.get('on_delay', None)
+        self.off_delay = self.args.get('off_delay', None)
 
         self.listen_state(self.trigger_on, self.trigger_entity, new='on', old='off')
         self.listen_state(self.trigger_off, self.trigger_entity, new='off', old='on')
 
     def trigger_on(self, entity, attribute, old, new, kwargs):
+        if self.on_delay is None:
+            self.trigger_on_delayed()
+        else:
+            self.timer_on = self.run_in(callback=self.trigger_on_delayed, seconds=int(self.on_delay))
+
+    def trigger_on_delayed(self, *args, **kwargs):
         for entity in self.action_entities:
             self.delayer.add(hass_func='turn_on', entity_id=entity)
+        self.timer_on = None
 
     def trigger_off(self, entity, attribute, old, new, kwargs):
+        # directly turn off if no delay is set or the start delay is not yet over
+        if self.off_delay is None or self.timer_on is not None:
+            self.trigger_off_delayed()
+        else:
+            self.timer_off = self.run_in(callback=self.trigger_off_delayed, seconds=int(self.off_delay))
+
+    def trigger_off_delayed(self, *args, **kwargs):
         for entity in self.action_entities:
             self.delayer.add(hass_func='turn_off', entity_id=entity)
+        self.timer_off = None
+
+        # cancel on timer to prevent delayed activation
+        if self.timer_on is not None:
+            self.cancel_timer(self.timer_on)
+            self.timer_on = None
