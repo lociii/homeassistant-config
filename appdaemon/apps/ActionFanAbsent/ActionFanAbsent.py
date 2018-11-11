432d673b669830fd053427027f49c8877405f0e8
Jens Nistler <opensource@jensnistler.de>
2018-11-11 15:53:28 +0100

    verbose logging, reset cycle timer correctly

--- appdaemon/apps/ActionFanAbsent/ActionFanAbsent.py
+++ appdaemon/apps/ActionFanAbsent/ActionFanAbsent.py
@@ -14,12 +14,17 @@ class ActionFanAbsent(hass.Hass):
         self.listen_state(self.returning, self.indicator_presence, new='True', old='False')
 
     def leaving(self, *args, **kwargs):
+        self.log('leaving - starting absence fan cycle for {}'.format(self.fan))
         self.absent_turn_on()
 
     def returning(self, *args, **kwargs):
+        self.log('returning - ending absence fan cycle for {}'.format(self.fan))
+
         if self.timer_cycle is not None:
             self.cancel_timer(self.timer_cycle)
-            self.delayer.add(hass_func='turn_off', entity_id=self.fan)
+            self.timer_cycle = None
+
+        self.delayer.add(hass_func='turn_off', entity_id=self.fan)
 
     def absent_turn_on(self, *args, **kwargs):
         self.log('absence fan cycle - turn fan {} on'.format(self.fan))
