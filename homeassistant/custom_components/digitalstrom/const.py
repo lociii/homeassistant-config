d765fcb9d8685c0558eae9370872edf61b4ce23e
Jens Nistler <opensource@jensnistler.de>
2019-08-17 16:05:32 +0200

    manage all custom components by hacs

--- homeassistant/custom_components/digitalstrom/const.py
+++
@@ -1,8 +0,0 @@
-"""Define constants for the digitalSTROM component."""
-
-CONF_APARTMENT = 'apartment'
-
-DOMAIN = 'digitalstrom'
-DOMAIN_LISTENER = DOMAIN + '_listener'
-
-CONFIG_PATH = '.digitalstrom/auth.json'
