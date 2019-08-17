f37c11a5f1b37700e9ec31289294831fc8ec11a1
Jens Nistler <opensource@jensnistler.de>
2019-08-17 14:04:36 +0200

    add occupancy automation for shower

--- appdaemon/apps/ActionLight/ActionLight.py
+++ appdaemon/apps/ActionLight/ActionLight.py
@@ -1,24 +1,31 @@
-import appdaemon.plugins.hass.hassapi as hass
+from globals import AppDaemon
 
 
-class ActionLight(hass.Hass):
+class ActionLight(AppDaemon):
     def initialize(self):
         self.delayer = self.get_app('util_delayer')
 
         self.trigger_entity = self.args['trigger_entity']
         self.trigger_status = self.args['trigger_status']
-        self.action_entities = self.args['action_entities']
-        self.action_status = self.args['action_status']
+        self.action_scenes = self.args.get('action_scenes', [])
+        self.action_entities = self.args.get('action_entities', [])
+        self.action_status = self.args.get('action_status', False)
         self.action_flash = self.args['action_flash'] if 'action_flash' in self.args else False
 
         status_text = 'on' if self.trigger_status else 'off'
-        self.listen_state(cb=self.update_status, entity=self.trigger_entity, new=status_text)
+        self.listen_state(cb=self.update_status, entity=self.trigger_entity, new=status_text,
+                          check_constraint_list=True)
 
     def update_status(self, *args, **kwargs):
         self.log('{} turned {}'.format(self.trigger_entity, self.trigger_status))
 
+        # handle all entity changes
         for entity in self.action_entities:
             if self.action_status:
                 self.delayer.add(hass_func='turn_on', entity_id=entity)
             else:
                 self.delayer.add(hass_func='turn_off', entity_id=entity)
+
+        # execute all scenes
+        for scene in self.action_scenes:
+            self.delayer.add(hass_func='turn_on', entity_id=scene)
