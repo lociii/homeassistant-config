f2a1eb0db37c5fc022c328d521083874693a511e
Jens Nistler <opensource@jensnistler.de>
2019-09-11 22:14:52 +0200

    query previous state on cover scenes

--- appdaemon/apps/ActionCover/ActionCover.py
+++ appdaemon/apps/ActionCover/ActionCover.py
@@ -6,17 +6,19 @@ class ActionCover(AppDaemon):
         self.delayer = self.get_app('util_delayer')
 
         self.trigger_entity = self.args['trigger_entity']
-        self.trigger_status = self.args['trigger_status']
+        self.trigger_status_to = self.args['trigger_status_to']
+        self.trigger_status_from = self.args.get('trigger_status_from', None)
         self.action_scenes = self.args.get('action_scenes', [])
         self.action_entities = self.args.get('action_entities', [])
         self.action_status = self.args.get('action_status', False)
 
-        status_text = 'on' if self.trigger_status else 'off'
-        self.listen_state(cb=self.update_status, entity=self.trigger_entity, new=status_text,
-                          check_constraint_list=True)
+        status_text_to = 'on' if self.trigger_status_to else 'off'
+        status_text_from = 'on' if self.trigger_status_from else 'off'
+        self.listen_state(cb=self.update_status, entity=self.trigger_entity, new=status_text_to,
+                          old=status_text_from, check_constraint_list=True)
 
     def update_status(self, *args, **kwargs):
-        self.log('{} turned {}'.format(self.trigger_entity, self.trigger_status))
+        self.log('{} turned {}'.format(self.trigger_entity, self.trigger_status_to))
 
         # handle all entity changes
         for entity in self.action_entities:
